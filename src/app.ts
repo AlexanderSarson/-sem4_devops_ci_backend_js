54b196627da04da8f32b17ebf1d38506753b10f6
Alexander <Alexander_Sarson@Outlook.com>
2020-11-12 14:29:06 +0100

    dummy2 api

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ app.get("/api/dummy", (req, res) => {
   res.json({ msg: "Hello" })
 })
 
+app.get("/api/dummy2", (req, res) => {
+  res.json({ msg: "Hello2" })
+})
+
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT)
 console.log(`Server started, listening on port: ${PORT}`)
